19ef52c5f77c66ebe5318047b33daad4beab822b
Marie Denney <mldenne@gmail.com>
2016-08-03 16:27:58 -0400

    Added comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'comments/body:text'
+
+  get 'comments/user_id:integer'
+
+  get 'comments/link_id:integer'
+
   resources :subreddits
   resources :users
   resources :links do

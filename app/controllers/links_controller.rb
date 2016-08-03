30aad8a3d565528e8f4c8834e2632913457e77c9
Marie Denney <mldenne@gmail.com>
2016-08-03 16:09:23 -0400

    Added link#show page

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -10,6 +10,7 @@ class LinksController < ApplicationController
   # GET /links/1
   # GET /links/1.json
   def show
+    @link = Link.find(params[:id])
   end
 
   # Rather than using show for this, maybe add a recent=true param to index? links?recent = true / if params[:recent]
